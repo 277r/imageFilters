b4ba140e861cee2d4643a9d98d718ef1a188bcb3
277r <rick_degraaf@hotmail.nl>
2021-11-02 11:48:10 +0100

    fixed indentation to tabs instead of 4 spaces

--- interpretasrgb.cpp
+++ interpretasrgb.cpp
@@ -1,34 +1,34 @@
 #include <png++/png.hpp>
 
 png::rgb_pixel to_rgb(png::rgb_pixel in){
-    png::rgb_pixel rValue;
-    long long r = ((long long)(66 * long(in.red) + 129 * long(in.green) + 25 * long(in.blue) + 128));
-    r >>= 8;
-    r += 16;
-    long long g = ((long long)(-38 * long(in.red) -74 * long(in.green) + 112 * long(in.blue) + 128)); 
-    g >>= 8;
-    g += 128;
-    long long b = ((long long)(112 * long(in.red) -94 * long(in.green) -18 * long(in.blue) + 128)); 
-    b >>= 8;
-    b += 128;
-    rValue.red = r;
-    rValue.green = g;
-    rValue.blue = b;
-    return rValue;
+	png::rgb_pixel rValue;
+	long long r = ((long long)(66 * long(in.red) + 129 * long(in.green) + 25 * long(in.blue) + 128));
+	r >>= 8;
+	r += 16;
+	long long g = ((long long)(-38 * long(in.red) -74 * long(in.green) + 112 * long(in.blue) + 128)); 
+	g >>= 8;
+	g += 128;
+	long long b = ((long long)(112 * long(in.red) -94 * long(in.green) -18 * long(in.blue) + 128)); 
+	b >>= 8;
+	b += 128;
+	rValue.red = r;
+	rValue.green = g;
+	rValue.blue = b;
+	return rValue;
 }
 
 
 int main(int argc, char *argv[]){
-    if (argc < 3){
-        return 0;
-    }
+	if (argc < 3){
+		return 0;
+	}
 
-    png::image< png::rgb_pixel > image(argv[1]);
-    for (int i = 0; i < image.get_height(); i++){
-        for (int ii = 0; ii < image.get_width(); ii++){
-            image[i][ii] = to_rgb(image[i][ii]);
-        }
-    }
-    
-    image.write(argv[2]);
+	png::image< png::rgb_pixel > image(argv[1]);
+	for (int i = 0; i < image.get_height(); i++){
+		for (int ii = 0; ii < image.get_width(); ii++){
+			image[i][ii] = to_rgb(image[i][ii]);
+		}
+	}
+	
+	image.write(argv[2]);
 }
--- interpretasyuv.cpp
+++ interpretasyuv.cpp
@@ -1,25 +1,25 @@
 #include <png++/png.hpp>
 
 png::rgb_pixel to_yuv(png::rgb_pixel in){
-    png::rgb_pixel rValue;
-    rValue.red = long(0.299 * long(in.red) + 0.587 * long(in.green) + 0.114 * long(in.blue)) & 255;
-    rValue.green = long(-0.147 * long(in.red) - 0.289 * long(in.green) + 0.436 * long(in.blue)) & 255;
-    rValue.blue = long(0.651 * long(in.red) - 0.515 * long(in.green) - 0.100 * long(in.blue)) & 255;
-    return rValue;
+	png::rgb_pixel rValue;
+	rValue.red = long(0.299 * long(in.red) + 0.587 * long(in.green) + 0.114 * long(in.blue)) & 255;
+	rValue.green = long(-0.147 * long(in.red) - 0.289 * long(in.green) + 0.436 * long(in.blue)) & 255;
+	rValue.blue = long(0.651 * long(in.red) - 0.515 * long(in.green) - 0.100 * long(in.blue)) & 255;
+	return rValue;
 }
 
 
 int main(int argc, char *argv[]){
-    if (argc < 3){
-        return 0;
-    }
+	if (argc < 3){
+		return 0;
+	}
 
-    png::image< png::rgb_pixel > image(argv[1]);
-    for (int i = 0; i < image.get_height(); i++){
-        for (int ii = 0; ii < image.get_width(); ii++){
-            image[i][ii] = to_yuv(image[i][ii]);
-        }
-    }
-    
-    image.write(argv[2]);
+	png::image< png::rgb_pixel > image(argv[1]);
+	for (int i = 0; i < image.get_height(); i++){
+		for (int ii = 0; ii < image.get_width(); ii++){
+			image[i][ii] = to_yuv(image[i][ii]);
+		}
+	}
+	
+	image.write(argv[2]);
 }
